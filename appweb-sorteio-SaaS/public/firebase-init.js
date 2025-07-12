03e05530e0ab4a1fafef178d9b86369d2a2c43b1
Rodrigo Borges <62719386+ClaudesonRodrigo@users.noreply.github.com>
2025-07-12 15:14:03 -0300

    Update firebase-init.js

--- appweb-sorteio-SaaS/public/firebase-init.js
+++ appweb-sorteio-SaaS/public/firebase-init.js
@@ -1,18 +1,21 @@
-// public/firebase-init.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Importa a função de inicialização do SDK do Firebase.
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-
-// A sua configuração do Firebase, agora num único local.
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCNFkoa4Ark8R2uzhX95NlV8Buwg2GHhvo",
-    authDomain: "cemvezesmais-1ab48.firebaseapp.com",
-    projectId: "cemvezesmais-1ab48",
-    storageBucket: "cemvezesmais-1ab48.firebasestorage.app",
-    messagingSenderId: "206492928997",
-    appId: "1:206492928997:web:763cd52f3e9e91a582fd0c",
-    measurementId: "G-G3BX961SHY"
+  apiKey: "AIzaSyAZUNUQvG-mLePJridVY_r1q9tW0xGRy00",
+  authDomain: "appweb-sorteio-saas-3e7c7.firebaseapp.com",
+  projectId: "appweb-sorteio-saas-3e7c7",
+  storageBucket: "appweb-sorteio-saas-3e7c7.firebasestorage.app",
+  messagingSenderId: "969938263965",
+  appId: "1:969938263965:web:c605099e5ea9d02239d974",
+  measurementId: "G-DL960DSWT4"
 };
 
-// Inicializa o Firebase e exporta a instância do 'app' para ser usada em outros arquivos.
-export const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
